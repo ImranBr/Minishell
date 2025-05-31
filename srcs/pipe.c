a439ae6493cc46de44cc0e57a231bee4bd18f635
Imran Barbouch <imranbarbouch@hotmail.fr>
2025-05-31 19:03:52 +0200

    pwd + fix pipe

--- srcs/pipe.c
+++ srcs/pipe.c
@@ -6,34 +6,36 @@
 /*   By: ibarbouc <ibarbouc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 16:40:59 by ibarbouc          #+#    #+#             */
-/*   Updated: 2025/05/31 15:57:55 by ibarbouc         ###   ########.fr       */
+/*   Updated: 2025/05/31 19:03:44 by ibarbouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void    syntax_pipe(char *input)
+void	syntax_pipe(char *input)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (input[i] == ' ')
-        i++;
-    if(input[i] == '|')
-    {
-        ft_putendl_fd("syntax error near unexpected token `|'", STDERR_FILENO);
-        return ;
-    }
-    i = ft_strlen(input) - 1;
-    while (i >= 0 && i == ' ')
-        i--;
-    if (i >= 0 && input[i] == '|')
-        ft_putendl_fd("bash: syntax error: unexpected end of file", STDERR_FILENO);
-    i = 0;
-    while (input[i] && input[i +1])
-    {
-        if(input[i] == '|' && input[i + 1] == '|')
-            ft_putendl_fd("syntax error near unexpected token `|'", STDERR_FILENO);
-        i++;
-    }
+	i = 0;
+	while (input[i] == ' ')
+		i++;
+	if (input[i] == '|')
+	{
+		ft_putendl_fd("syntax error near unexpected token `|'", STDERR_FILENO);
+		return ;
+	}
+	i = ft_strlen(input) - 1;
+	while (i >= 0 && i == ' ')
+		i--;
+	if (i >= 0 && input[i] == '|')
+		ft_putendl_fd("bash: syntax error: unexpected end of file",
+			STDERR_FILENO);
+	i = 0;
+	while (input[i] && input[i + 1])
+	{
+		if (input[i] == '|' && input[i + 1] == '|')
+			ft_putendl_fd("syntax error near unexpected token `|'",
+				STDERR_FILENO);
+		i++;
+	}
 }
