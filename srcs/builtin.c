09d81614b7074bba5351b70322ae44b28336c375
Imran Barbouch <imranbarbouch@hotmail.fr>
2025-06-01 19:13:39 +0200

    builtin echo

--- srcs/builtin.c
+++ srcs/builtin.c
@@ -6,60 +6,60 @@
 /*   By: ibarbouc <ibarbouc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/31 16:45:44 by ibarbouc          #+#    #+#             */
-/*   Updated: 2025/06/01 19:09:36 by ibarbouc         ###   ########.fr       */
+/*   Updated: 2025/06/01 19:13:32 by ibarbouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int builtin_pwd()
+int	builtin_pwd(void)
 {
-    char *cwd;
-    cwd = getcwd(NULL, 0);
-    if (!cwd)
-    {
-        perror("pwd");
-        return (1);
-    }
-    printf("%s\n", cwd);
-    free(cwd);
-    return (0);
+	char	*cwd;
+
+	cwd = getcwd(NULL, 0);
+	if (!cwd)
+	{
+		perror("pwd");
+		return (1);
+	}
+	printf("%s\n", cwd);
+	free(cwd);
+	return (0);
 }
 
-int builtin_env(char **env)
+int	builtin_env(char **env)
 {
-    int i;
-    i = 0;
+	int	i;
 
-    while (env && env[i])
-    {
-        printf("%s\n", env[i]);
-        i++;
-    }
-    return (0);
+	i = 0;
+	while (env && env[i])
+	{
+		printf("%s\n", env[i]);
+		i++;
+	}
+	return (0);
 }
 
-int builtin_echo(char **args)
+int	builtin_echo(char **args)
 {
-    int i;
-    int newline;
-    
-    i = 1;
-    newline = 1;
-    
-    if (args[i] && ft_strncmp(args[i], "-n", 3) == 0)
-    {
-        newline = 0;
-        i++;
-    }
-    while (args[i])
-    {
-        printf("%s", args[i]);
-        if (args[i + 1])
-            printf(" ");
-        i++;
-    }
-    if (newline)
-        printf("\n");
-    return (0);
+	int	i;
+	int	newline;
+
+	i = 1;
+	newline = 1;
+	if (args[i] && ft_strncmp(args[i], "-n", 3) == 0)
+	{
+		newline = 0;
+		i++;
+	}
+	while (args[i])
+	{
+		printf("%s", args[i]);
+		if (args[i + 1])
+			printf(" ");
+		i++;
+	}
+	if (newline)
+		printf("\n");
+	return (0);
 }
