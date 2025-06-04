03a69904af4d33f12bfcd571c99597f14dd9b312
Imran Barbouch <imranbarbouch@hotmail.fr>
2025-06-04 14:21:12 +0200

    special char

--- srcs/caractere.c
+++ srcs/caractere.c
@@ -6,7 +6,7 @@
 /*   By: ibarbouc <ibarbouc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 12:46:26 by ibarbouc          #+#    #+#             */
-/*   Updated: 2025/06/04 14:17:06 by ibarbouc         ###   ########.fr       */
+/*   Updated: 2025/06/04 14:20:52 by ibarbouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,3 @@ int	is_special_char(char c)
 	return (c == '|' || c == '&' || c == ';' || c == '>' || c == '<');
 }
 
-int	neutralize_special_char_in_quote(char *input)
-{
-	int	i;
-	int	in_single;
-
-	i = 0;
-}
